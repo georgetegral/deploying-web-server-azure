6529fdc13213983c2d21c1d5fb322fafbc1c6a25
georgetegral <georgetegral@gmail.com>
2021-06-10 02:37:33 -0500

    Added necessary variables to vars.tf

--- vars.tf
+++ vars.tf
@@ -4,13 +4,13 @@ variable "prefix" {
 }
 
 variable "environment" {
-    description = "The environment which should be used for all resources in this example."
-    default = "udacity"
+  description = "The environment which should be used for all resources in this example."
+  default = "dev"
 }
 
 variable "vm_num"{
-    description = "The number of virtual machines to be deployed in this example."
-    default = "2"
+  description = "The number of virtual machines to be deployed in this example."
+  default = "2"
 }
 
 variable "location" {
@@ -19,9 +19,16 @@ variable "location" {
 }
 
 variable "username" {
-    description = "username for the virtual machine."
+  description = "username for the virtual machine."
+  default = "udacityuser"
 }
 
 variable "password" {
-    description = "password for the virtual machine."
+  description = "password for the virtual machine."
+  default = "Udacity1Pass2@"
+}
+
+variable "image"{
+  description = "The Packer Image location in Azure"
+  default = "/subscriptions/76ecc65a-b866-4328-925f-0cafa9642559/resourceGroups/udacity-rg/providers/Microsoft.Compute/images/PackerImage"
 }
